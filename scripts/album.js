1f62fedbc475e9696eac9599f5d525a6c4d9bbb3
Angie Gingerich <macuser@MacUsers-MacBook-Pro.local>
2016-10-18 10:27:45 -0600

    Small assignment 13 update

--- scripts/album.js
+++ scripts/album.js
@@ -61,6 +61,7 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
+        var currentParent = element.parentElement;
         if (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
